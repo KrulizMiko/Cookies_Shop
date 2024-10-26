f1289b0e06f50927ce403b05b621370422e63abd
Артем <168135876+KrulizMiko@users.noreply.github.com>
2024-10-26 14:16:32 +0400

    2 or 3 update
    Сделан лейндинг сайт только на html и css. Следующий коммит будет с внедрнением js

---
+++ cookies_landing/index.js
@@ -0,0 +1,7 @@
+const buyButton = document.getElementById('buyBtn');
+
+buyButton.addEventListener('click', () => {
+    setTimeout(function () {
+        window.location.href = "https://ya.ru/";
+    }, 4000);
+});
